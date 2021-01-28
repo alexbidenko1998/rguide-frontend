5f294bda6a889fb6e0124f538b969569f2df5569
Alexander <alexbidenko1998@gmail.com>
2021-01-28 17:25:15 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="text-center">
+        <logo />
+        <vuetify-logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline"> Welcome to RGuide</v-card-title>
+        <v-card-text>
+          <p>Лучший 3D справочник технического оборудования.</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/inspire">Начать изучение</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo,
+  },
+}
+</script>
