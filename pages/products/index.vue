c3834ef64e1427ce5a64cc8e14d3d5cf87c1527c
Alexander <alexbidenko1998@gmail.com>
2021-03-30 14:29:37 +0300

    Create category filtering

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,14 +1,21 @@
 <template>
   <v-row class="productsPage">
     <v-col class="text-center">
+      <v-tabs
+        v-if="!search"
+        v-model="categoryIndex"
+        show-arrows
+        class="mb-4"
+      >
+        <v-tab v-for="c in categories" :key="c.id">{{ c.title }}</v-tab>
+      </v-tabs>
+
       <v-text-field
         v-model="search"
         label="Поиск"
         clearable
         class="productsPage__search"
       />
-
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
 
       <Card v-for="product in products" :key="product.id" :product="product" />
     </v-col>
@@ -22,23 +29,48 @@ export default {
   components: { Card },
 
   data: () => ({
+    categories: [],
     products: [],
+    categoryIndex: 0,
     search: '',
     timer: null,
   }),
 
   async fetch() {
-    this.products = await this.$axios.$get('/products')
+    this.categories = await this.$axios.$get('/categories')
+    this.products = await this.$axios.$get('/products', {
+      params: { category_id: this.categories[0].id },
+    })
   },
 
   watch: {
     search(val) {
       clearTimeout(this.timer)
       setTimeout(() => {
-        this.$axios.$get(`/products?q=${val || ''}`).then((products) => {
+        this.$axios
+          .$get('/products', {
+            params: {
+              q: val,
+              category_id: val
+                ? undefined
+                : this.categories[this.categoryIndex].id,
+            },
+          })
+          .then((products) => {
+            this.products = products
+          })
+      }, 500)
+    },
+    categoryIndex(val) {
+      this.$axios
+        .$get('/products', {
+          params: {
+            category_id: this.categories[val].id,
+          },
+        })
+        .then((products) => {
           this.products = products
         })
-      }, 500)
     },
   },
 }
@@ -49,7 +81,7 @@ export default {
   height: 100%;
   overflow: auto;
 
-  &__search {
+  & &__search {
     max-width: 374px;
     margin-left: auto;
     margin-right: auto;
